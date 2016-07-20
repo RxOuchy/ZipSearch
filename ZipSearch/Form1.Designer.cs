a1abcb6c0c34220caefc69b3dfa1bc5f283b7205
Brandon <bwhite@listrak.com>
2016-07-19 22:05:37 -0400

    Initial Submit

--- ZipSearch/Form1.Designer.cs
+++ ZipSearch/Form1.Designer.cs
@@ -28,12 +28,303 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.txtExtract = new System.Windows.Forms.TextBox();
+            this.txtClient = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.txtFileExt = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dtStart = new System.Windows.Forms.DateTimePicker();
+            this.dtEnd = new System.Windows.Forms.DateTimePicker();
+            this.btnSearchFiles = new System.Windows.Forms.Button();
+            this.grdFiles = new System.Windows.Forms.DataGridView();
+            this.btnExtractFiles = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnExtract = new System.Windows.Forms.Button();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdFiles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(12, 4, 0, 4);
+            this.menuStrip1.Size = new System.Drawing.Size(1254, 44);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(64, 36);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 38);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 71);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Search Directory";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 179);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Extract Directory";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 288);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "ClientUID";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(26, 104);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(6);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(466, 31);
+            this.txtSearch.TabIndex = 4;
+            // 
+            // txtExtract
+            // 
+            this.txtExtract.Location = new System.Drawing.Point(26, 212);
+            this.txtExtract.Margin = new System.Windows.Forms.Padding(6);
+            this.txtExtract.Name = "txtExtract";
+            this.txtExtract.Size = new System.Drawing.Size(466, 31);
+            this.txtExtract.TabIndex = 5;
+            // 
+            // txtClient
+            // 
+            this.txtClient.Location = new System.Drawing.Point(26, 321);
+            this.txtClient.Margin = new System.Windows.Forms.Padding(6);
+            this.txtClient.Name = "txtClient";
+            this.txtClient.Size = new System.Drawing.Size(466, 31);
+            this.txtClient.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 408);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "File Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(290, 408);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(148, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "File Extension";
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(26, 440);
+            this.txtFileName.Margin = new System.Windows.Forms.Padding(6);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(196, 31);
+            this.txtFileName.TabIndex = 9;
+            // 
+            // txtFileExt
+            // 
+            this.txtFileExt.Location = new System.Drawing.Point(296, 440);
+            this.txtFileExt.Margin = new System.Windows.Forms.Padding(6);
+            this.txtFileExt.Name = "txtFileExt";
+            this.txtFileExt.Size = new System.Drawing.Size(196, 31);
+            this.txtFileExt.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(26, 513);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 25);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Start Date";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(296, 513);
+            this.label7.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(181, 25);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "End Date (Today)";
+            // 
+            // dtStart
+            // 
+            this.dtStart.CustomFormat = "";
+            this.dtStart.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtStart.Location = new System.Drawing.Point(26, 546);
+            this.dtStart.Margin = new System.Windows.Forms.Padding(6);
+            this.dtStart.Name = "dtStart";
+            this.dtStart.Size = new System.Drawing.Size(196, 31);
+            this.dtStart.TabIndex = 13;
+            // 
+            // dtEnd
+            // 
+            this.dtEnd.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtEnd.Location = new System.Drawing.Point(302, 546);
+            this.dtEnd.Margin = new System.Windows.Forms.Padding(6);
+            this.dtEnd.Name = "dtEnd";
+            this.dtEnd.Size = new System.Drawing.Size(190, 31);
+            this.dtEnd.TabIndex = 14;
+            // 
+            // btnSearchFiles
+            // 
+            this.btnSearchFiles.Location = new System.Drawing.Point(344, 644);
+            this.btnSearchFiles.Margin = new System.Windows.Forms.Padding(6);
+            this.btnSearchFiles.Name = "btnSearchFiles";
+            this.btnSearchFiles.Size = new System.Drawing.Size(150, 44);
+            this.btnSearchFiles.TabIndex = 15;
+            this.btnSearchFiles.Text = "Search";
+            this.btnSearchFiles.UseVisualStyleBackColor = true;
+            this.btnSearchFiles.Click += new System.EventHandler(this.btnSearchFiles_Click);
+            // 
+            // grdFiles
+            // 
+            this.grdFiles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdFiles.Location = new System.Drawing.Point(602, 71);
+            this.grdFiles.Margin = new System.Windows.Forms.Padding(6);
+            this.grdFiles.Name = "grdFiles";
+            this.grdFiles.Size = new System.Drawing.Size(624, 512);
+            this.grdFiles.TabIndex = 16;
+            // 
+            // btnExtractFiles
+            // 
+            this.btnExtractFiles.Location = new System.Drawing.Point(970, 644);
+            this.btnExtractFiles.Margin = new System.Windows.Forms.Padding(6);
+            this.btnExtractFiles.Name = "btnExtractFiles";
+            this.btnExtractFiles.Size = new System.Drawing.Size(254, 44);
+            this.btnExtractFiles.TabIndex = 17;
+            this.btnExtractFiles.Text = "Extract Selected";
+            this.btnExtractFiles.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(501, 104);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(48, 38);
+            this.btnSearch.TabIndex = 18;
+            this.btnSearch.Text = "...";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnExtract
+            // 
+            this.btnExtract.Location = new System.Drawing.Point(501, 212);
+            this.btnExtract.Name = "btnExtract";
+            this.btnExtract.Size = new System.Drawing.Size(48, 36);
+            this.btnExtract.TabIndex = 20;
+            this.btnExtract.Text = "...";
+            this.btnExtract.UseVisualStyleBackColor = true;
+            this.btnExtract.Click += new System.EventHandler(this.btnExtract_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1254, 709);
+            this.Controls.Add(this.btnExtract);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnExtractFiles);
+            this.Controls.Add(this.grdFiles);
+            this.Controls.Add(this.btnSearchFiles);
+            this.Controls.Add(this.dtEnd);
+            this.Controls.Add(this.dtStart);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtFileExt);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtClient);
+            this.Controls.Add(this.txtExtract);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdFiles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.TextBox txtExtract;
+        private System.Windows.Forms.TextBox txtClient;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.TextBox txtFileExt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DateTimePicker dtStart;
+        private System.Windows.Forms.DateTimePicker dtEnd;
+        private System.Windows.Forms.Button btnSearchFiles;
+        private System.Windows.Forms.DataGridView grdFiles;
+        private System.Windows.Forms.Button btnExtractFiles;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnExtract;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
     }
 }
 
--- ZipSearch/Form1.cs
+++ ZipSearch/Form1.cs
@@ -1,20 +1,117 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace ZipSearch
 {
     public partial class Form1 : Form
     {
+
+        List<string> filesFound = new List<string>();
+
         public Form1()
         {
             InitializeComponent();
+            this.Width = 300; //Half Size
+            //this.Width = 645; //Full Size
+            dtStart.Value = DateTime.Now.AddDays(-30);
+
         }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private bool ValidateForm()
+        {
+            List<string> errors = new List<string>();
+
+            if (String.IsNullOrEmpty(txtSearch.Text) || txtSearch.Text == "Please select a search location")
+            {
+                txtSearch.Text = "Please select a search location";
+                txtSearch.BackColor = Color.FromArgb(237,88,71);
+                errors.Add("Please select a search location");
+            }
+
+            if (String.IsNullOrEmpty(txtExtract.Text) || txtExtract.Text == "Please select an extraction location")
+            {
+                txtExtract.Text = "Please select an extraction location";
+                txtExtract.BackColor = Color.FromArgb(237, 88, 71);
+                errors.Add("Please select an extraction location");
+            }
+
+            if (String.IsNullOrEmpty(txtClient.Text) || txtClient.Text == "Please enter the Client UID")
+            {
+                txtClient.Text = "Please enter the Client UID";
+                txtClient.BackColor = Color.FromArgb(237, 88, 71);
+                errors.Add("Please enter the Client UID");
+            }
+
+            if (String.IsNullOrEmpty(txtFileName.Text) || txtFileName.Text == "Please enter a file name (Product, customers, orders, etc)")
+            {
+                txtFileName.Text = "Please enter a file name (Product, customers, orders, etc)";
+                txtFileName.BackColor = Color.FromArgb(237, 88, 71);
+                errors.Add("Please enter a file name (Product, customers, orders, etc)");
+            }
+
+            if (String.IsNullOrEmpty(txtFileExt.Text) || txtFileExt.Text == "Please enter a File Extension (csv, txt, etc)")
+            {
+                txtFileExt.Text = "Please enter a File Extension (csv, txt, etc)";
+                txtFileExt.BackColor = Color.FromArgb(237, 88, 71);
+                errors.Add("Please enter a File Extension (csv, txt, etc)");
+            }
+
+            return errors.Count == 0;
+        }
+
+        private void btnSearchFiles_Click(object sender, EventArgs e)
+        {
+            if (ValidateForm())
+            {
+
+                string zipPath = txtSearch.Text;
+                string searchString = txtClient.Text;
+                string fileName = txtFileName.Text + "*." + txtFileExt.Text;
+
+                foreach (var filePath in Directory.EnumerateFiles(zipPath, "*.zip"))
+                {
+                    using (ZipArchive archive = ZipFile.OpenRead(filePath))
+                    {
+                        foreach (ZipArchiveEntry entry in archive.Entries)
+                        {
+                            if (entry.FullName.StartsWith(searchString, StringComparison.OrdinalIgnoreCase))
+                            {
+                                string reg = Regex.Escape(fileName).Replace("\\?", ".").Replace("\\*", ".*") + "$";
+                                if (Regex.IsMatch(entry.FullName, reg))
+                                {
+                                    filesFound.Add(entry.FullName);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                this.Width = 645;
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+                txtSearch.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void btnExtract_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog2.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+                txtExtract.Text = folderBrowserDialog2.SelectedPath;
+        }
+
     }
 }
